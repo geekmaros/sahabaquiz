3246e33962706a9953efb1e712517147924b4baf
Mustapha Abdul-Rasaq <razicruz@gmail.com>
2024-10-06 19:49:03 +0100

    feat: add highscore to quiz

--- src/components/App.js
+++ src/components/App.js
@@ -8,13 +8,17 @@ import Loader from "./Loader";
 import Error from "./Error";
 import StartScreen from "./StartScreen";
 import Question from "./Question";
+import NextButton from "./NextButton";
+import Progress from "../Progress";
+import FinishScreen from "./FinishScreen";
 
 const initialState = {
   questions: [],
   status: "loading",
   index: 0,
   answer: null,
-  points: 0
+  points: 0,
+  highScore: 0
 };
 
 function reducer(state, action) {
@@ -43,13 +47,29 @@ function reducer(state, action) {
         answer: action.payload,
         points: action.payload === question.correctOption ? state.points + question.points : state.points
       };
+    case "nextQuestion":
+
+      return {
+        ...state,
+       index: state.index + 1,
+        answer: null
+      };
+    case "finish":
+
+      return {
+        ...state,
+       status: 'finished',
+        highScore: state.points > state.highScore ? state.points : state.highScore
+      };
     default:
       throw new Error("invalid action");
   }
 }
 
 function App() {
-  const [{ answer, index, questions, status }, dispatch] = useReducer(reducer, initialState);
+  const [{ answer, index, points, questions, status, highScore }, dispatch] = useReducer(reducer, initialState);
+
+  const maxPossiblePoints = questions.reduce((prev, cur) => prev + cur.points, 0)
 
   const numQuestions = questions.length;
 
@@ -71,7 +91,16 @@ function App() {
         {status === "ready" && (
           <StartScreen dispatch={dispatch} numQuestions={numQuestions} />
         )}
-        {status === "active" && <Question answer={answer} dispatch={dispatch}  questions={questions[index]} /> }
+        {status === "active" &&(
+            <>
+              <Progress answer={answer} index={index} points={points} maxPossiblePoints={maxPossiblePoints} numQuestion={numQuestions} />
+            <Question answer={answer} dispatch={dispatch}  questions={questions[index]} />
+        <NextButton dispatch={dispatch} index={index} numQuestions={numQuestions} answer={answer} />
+            </>
+            )}
+
+        {status === 'finished' && <FinishScreen points={points} highScore={highScore} maxPossiblePoints={maxPossiblePoints} />}
+
       </Main>
     </div>
   );
---
+++ src/components/FinishScreen.js
@@ -0,0 +1,28 @@
+import progress from "../Progress";
+
+function FinishScreen({highScore, maxPossiblePoints, points}) {
+    const percentage = (points/maxPossiblePoints) * 100
+
+    let emoji;
+    if(percentage === 100) emoji = "Maa Shaa Allah🥇"
+    if(percentage >=80 && percentage < 100) emoji = "Baraka Llahu fih🥈"
+    if(percentage >=50 && percentage < 80) emoji = "Allahuma Barik🤩"
+    if(percentage >=0 && percentage < 50) emoji = "Subhana Llah 🤨"
+    if(percentage === 0 && percentage < 50) emoji = "Inna liLlahi wa inna ilayhi raajium 🤦🏽‍♂️"
+
+    return (
+      <>
+        <p className={"result"}>
+          <span>{emoji}</span>You Scored{" "}
+          <strong>
+            {points} out of possible {maxPossiblePoints} (
+            {Math.ceil(percentage)}%)
+          </strong>
+        </p>
+        <p className="highscore">(Highscore: {highScore} points)</p>
+      </>
+    );
+
+}
+
+export default FinishScreen
