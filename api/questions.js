843dd5f57548aa15e04bad5dc5de868306373849
Mustapha Abdul-Rasaq <razicruz@gmail.com>
2024-10-07 12:57:18 +0100

    feat: test questions endpoint for vercel

--- api/questions.js
+++ api/questions.js
@@ -3,5 +3,6 @@ import ques from  "data/questions.json"
 export default function questions(req, res) {
     res.statusCode = 200;
     const data = ques
+    console.log(data)
     res.json({ data });
 }
