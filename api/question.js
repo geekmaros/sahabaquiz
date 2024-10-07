f0b9ffb008a9ca52b10976ea5a8d5a5576bb1df8
Mustapha Abdul-Rasaq <razicruz@gmail.com>
2024-10-07 02:49:52 +0100

    feat: test for vercel

--- api/question.js
+++ api/question.js
@@ -1,5 +1,8 @@
 import questions from  "data/questions.json"
 
-export default function handler(req, res){
-    res.status(200).json(questions)
+export const dynamic = 'force-dynamic'; // static by default, unless reading the request
+
+export default function handler(req, res) {
+    // Send the questions from the local JSON file
+    res.status(200).json(questions);
 }
